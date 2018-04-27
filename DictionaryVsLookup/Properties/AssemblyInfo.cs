4b1a93fa31a21f16c70d7f716b9ac09d08cdf175
Kevin Halverson <kevinh@microsoft.com>
2018-04-26 22:50:53 -0700

    Benchmark tests for Dictionary vs Lookup...

---
+++ DictionaryVsLookup/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DictionaryVsLookup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DictionaryVsLookup")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c02fba07-2224-432d-b5c3-410a49558417")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DictionaryVsLookup/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DictionaryVsLookup
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDictionary()
+        {
+            var teamProperties = GetUserJoinedTeams();
+            var teamDictionary = teamProperties.ToDictionary(p => p.Id);
+            var ownedGroup = GetOwnedGroups(teamProperties);
+
+            List<Tuple<int, DirectoryTeam>> list = new List<Tuple<int, DirectoryTeam>>(ownedGroup.Count);
+            list.AddRange(ownedGroup.Select((group) =>
+                CreateClassFromGroupAndTeam(group, teamDictionary)
+            ));
+
+            Assert.AreEqual(10, list.Count);
+        }
+
+        [TestMethod]
+        public void TestLookup()
+        {
+            var teamProperties = GetUserJoinedTeams();
+            var teamLookup = teamProperties.ToLookup(p => p.Id);
+            var ownedGroup = GetOwnedGroups(teamProperties);
+
+            List<Tuple<int, DirectoryTeam>> list = new List<Tuple<int, DirectoryTeam>>(ownedGroup.Count);
+            list.AddRange(ownedGroup.Select((group) =>
+                CreateClassFromGroupAndTeam(group, teamLookup)
+            ));
+
+            Assert.AreEqual(10, list.Count);
+        }
+
+        List<int> GetOwnedGroups(List<DirectoryTeam> allTeams)
+        {
+            var list = new List<int>();
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                list.Add(allTeams[Math.Abs(random.Next(9999))].Id);
+            }
+            return list;
+        }
+
+        List<DirectoryTeam> GetUserJoinedTeams()
+        {
+            var list = new List<DirectoryTeam>(10000);
+            var random = new Random();
+            for (var i = 0; i < 10000; i++)
+            {
+                list.Add(new DirectoryTeam { Id = random.Next() });
+            }
+            return list;
+        }
+
+        class DirectoryTeam
+        {
+            public int Id { get; set; }
+            public bool IsArchived { get; set; }
+        }
+
+        private static Tuple<int, DirectoryTeam> CreateClassFromGroupAndTeam(int group, Dictionary<int, DirectoryTeam> teamDictionary)
+        {
+            DirectoryTeam team;
+            Assert.IsTrue(teamDictionary.TryGetValue(group, out team));
+            return Tuple.Create(group, team);
+        }
+
+        private static Tuple<int, DirectoryTeam> CreateClassFromGroupAndTeam(int group, ILookup<int, DirectoryTeam> teamLookup)
+        {
+            DirectoryTeam team;
+            Assert.IsTrue(teamLookup.Contains(group));
+            team = teamLookup[group].First();
+            return Tuple.Create(group, team);
+        }
+    }
+}
